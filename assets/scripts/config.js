6e1a0ce5b54838649a9156378c89e8118eb75a30
eownie1-hue <eownie1@gmail.com>
2025-10-18 00:21:11 -0500

    Update config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -12,19 +12,9 @@ Config.Socials = [
 Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
 Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
 
-// Staff list
-Config.Staff = [
-    {name: "NaorNC #1", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
-    {name: "Owner #2", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
-
-
 ];
 
-// Categories
-Config.Categories = [
-    {label: "Social Media", default: true},
-    {label: "Staff", default: false}
-];
 
 // Music
+
 Config.Song = "song.mp3";
